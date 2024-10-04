f66287241a6e9bbc51d4a26f7da8d0b0d29ea2c8
CasManny <chukwusabastine2018@gmail.com>
2024-10-04 14:56:08 +0100

    correcting some styles

--- components/choose-adventure/GetUserFeedback.tsx
+++ components/choose-adventure/GetUserFeedback.tsx
@@ -6,7 +6,7 @@ import { useFeedbackModalStore } from "@/store/feedback-modal"
 const GetUserFeedback = () => {
     const { openFeedbackModal } = useFeedbackModalStore()
   return (
-      <Button className="rounded-full z-50 fixed bottom-2 right-2 animate-pulse" onClick={openFeedbackModal}>
+      <Button className="rounded-full z-50 fixed bottom-2 right-2 bg-rs-yellow text-white hover:bg-rs-yellow  hover:text-white" onClick={openFeedbackModal}>
           <MessageCircle className="w-h h-5 mr-2 animate-pulse" />
           Feedback
     </Button>
