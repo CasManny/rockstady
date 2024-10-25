16ba049e7e6e11023e0453facfc812d2688fe7e9
CasManny <chukwusabastine2018@gmail.com>
2024-10-25 16:14:08 +0100

    create leaderboardcard component

---
+++ app/(rock)/leaderboard/leaderboard-card.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Gem } from "lucide-react";
+
+interface LeaderboardCardProps {
+  positon: number;
+  gemNumber: number;
+  image: string | null;
+  name: string | null;
+}
+const LeaderboardCard = ({
+  positon,
+  gemNumber,
+  name,
+  image,
+}: LeaderboardCardProps) => {
+  const avatarFallback = name?.charAt(0).toUpperCase();
+
+  return (
+    <div className="flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50">
+      <p className="font-bold text-lime-700 mr-4">{positon + 1}</p>
+      <Avatar className="border bg-green-500 h-12 w-12 ml-3 mr-6">
+        {/* <AvatarImage src={image!} className="object-cover" /> */}
+        <AvatarFallback>{avatarFallback}</AvatarFallback>
+      </Avatar>
+      <p className="font-bold text-neutral-800 flex-1 truncate">{name}</p>
+      <div className="flex items-center gap-1">
+        <Gem className="w-5 h-5 sm:w-10 sm:h-10 text-rs-yellow" />
+        <p className="text-muted-foreground ">{gemNumber} Gem</p>
+      </div>
+    </div>
+  );
+};
+
+export default LeaderboardCard;
--- app/(rock)/leaderboard/page.tsx
+++ app/(rock)/leaderboard/page.tsx
@@ -1,73 +1,96 @@
-import FeedWrapper from '@/components/start-journey/FeedWrapper'
-import StickyWrapper from '@/components/start-journey/StickyWrapper'
-import UserProgressCard from '@/components/start-journey/UserProgressCard'
-import { Avatar, AvatarImage } from '@/components/ui/avatar'
-import { Separator } from '@/components/ui/separator'
-import { getLeaderboardData, getUserProgress } from '@/db/queries'
-import { Gem } from 'lucide-react'
-import Image from 'next/image'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import FeedWrapper from "@/components/start-journey/FeedWrapper";
+import StickyWrapper from "@/components/start-journey/StickyWrapper";
+import UserProgressCard from "@/components/start-journey/UserProgressCard";
+import { Separator } from "@/components/ui/separator";
+import { getLeaderboardData, getUserProgress } from "@/db/queries";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import React from "react";
+import LeaderboardCard from "./leaderboard-card";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const LeaderBoard = async () => {
-  const userProgressData = getUserProgress()
-  const leaderboardData = getLeaderboardData()
+  const userProgressData = getUserProgress();
+  const leaderboardData = getLeaderboardData();
 
-  const [userProgress, leaderboard] = await Promise.all([userProgressData, leaderboardData])
+  const [userProgress, leaderboard] = await Promise.all([
+    userProgressData,
+    leaderboardData,
+  ]);
 
-
+  if (!leaderboard || !userProgress) {
+    return (
+      <div className="flex flex-row-reverse gap-[48px] px-6">
+        <StickyWrapper>
+          <UserProgressCard
+            hearts={userProgress?.hearts}
+            points={userProgress?.points}
+          />
+        </StickyWrapper>
+        <FeedWrapper>
+          <div className="w-full flex flex-col items-center">
+            <Image
+              src={"/leaderboard.svg"}
+              alt="leaderboard"
+              height={90}
+              width={90}
+            />
+            <h1 className="tex-center font-bold text-neutral-800 text-2xl my-6">
+              Leaderboard
+            </h1>
+            <p className="text-muted-foreground text-center text-lg mb-6">
+              See where you stand among other learners in the community
+            </p>
+            <Separator className="mb-4 h-0.5 rounded-full" />
+            <div className="flex items-center justify-center flex-col">
+              <h1 className="text-2xl font-bold mb-4">Be the first to reach the top of the leaderboard!</h1>
+              <Button asChild>
+                <Link href={'/choose-an-adventure'}>Start Now</Link>
+              </Button>
+            </div>
+          </div>
+        </FeedWrapper>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
-    <StickyWrapper>
-      <UserProgressCard
-        hearts={userProgress?.hearts}
-        points={userProgress?.points}
-      />
-    </StickyWrapper>
-    <FeedWrapper>
-      <div className="w-full flex flex-col items-center">
-        <Image
-          src={"/leaderboard.svg"}
-          alt="leaderboard"
-          height={90}
-          width={90}
+      <StickyWrapper>
+        <UserProgressCard
+          hearts={userProgress?.hearts}
+          points={userProgress?.points}
         />
-        <h1 className="tex-center font-bold text-neutral-800 text-2xl my-6">
-          Leaderboard
-        </h1>
-        <p className="text-muted-foreground text-center text-lg mb-6">
-          See where you stand among other learners in the community
-        </p>
-        <Separator className="mb-4 h-0.5 rounded-full" />
-        {leaderboard.map((user, index) => (
-          <div
-            className="flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50"
-            key={index}
-          >
-            <p className="font-bold text-lime-700 mr-4">{index + 1}</p>
-            <Avatar className="border bg-green-500 h-12 w-12 ml-3 mr-6">
-              <AvatarImage
-                src={user?.userImage!}
-                className="object-cover"
-              />
-            </Avatar>
-            <p className="font-bold text-neutral-800 flex-1">
-              {user.userName}
-            </p>
-            <div className="flex items-center gap-1">
-              <Gem className='w-10 h-10 text-rs-yellow' />
-              <p className="text-muted-foreground ">
-                {user.points} Gem
-              </p>
-            </div>
-          </div>
-        ))}
-          {leaderboard.length === 0 && <h1 className='text-3xl font-semibold'>Leaderboard is Empty</h1>}
-      </div>
-    </FeedWrapper>
-  </div>
-  )
-}
+      </StickyWrapper>
+      <FeedWrapper>
+        <div className="w-full flex flex-col items-center">
+          <Image
+            src={"/leaderboard.svg"}
+            alt="leaderboard"
+            height={90}
+            width={90}
+          />
+          <h1 className="tex-center font-bold text-neutral-800 text-2xl my-6">
+            Leaderboard
+          </h1>
+          <p className="text-muted-foreground text-center text-lg mb-6">
+            See where you stand among other learners in the community
+          </p>
+          <Separator className="mb-4 h-0.5 rounded-full" />
+          {leaderboard.map((user, index: number) => (
+            <LeaderboardCard
+              key={index}
+              positon={index}
+              gemNumber={user.points}
+              image={user?.userImage}
+              name={user.userName}
+            />
+          ))}
+        </div>
+      </FeedWrapper>
+    </div>
+  );
+};
 
-export default LeaderBoard
+export default LeaderBoard;
